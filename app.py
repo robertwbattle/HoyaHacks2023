6ac49dbbfd73fed6b4cb634d579a6c418a936b6c
gitthing03 <86090190+gitthing03@users.noreply.github.com>
2023-01-27 22:59:19 -0500

    Index page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 app.run()
