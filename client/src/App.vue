46963bbf8860f9fa12edc753d2adc97996a180d1
DylanHalstead <dylanhalstead11@gmail.com>
2023-01-28 19:08:04 -0500

    gotten init graph, animations are in dev

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="main-container">
     <!-- <Micro /> -->
-    <Index msg="Test!"/>
-    <p> Howdy!</p>
     <FileForm url="http://127.0.0.1:5000/catch"></FileForm>
-    <p>fuidwsbdiusbfiuds</p>
     <span class="line"></span>
-    <p>fwiybdisubiuds</p>
-    <!-- <Macro /> -->
+    <Macro/>
   </div>
 </template>
 
@@ -15,12 +11,8 @@
   import Macro from './components/Macro'
   import Micro from './components/Micro'
   import Index from './components/Index.vue'
-import FileForm from './components/FileForm.vue'
+  import FileForm from './components/FileForm.vue'
 
-  import {parseJSON} from '@msagl/parser'
-  import {Renderer} from '@msagl/renderer'
-
-  import macroJSON from './json/exampleData.json'
 
 export default {
   name: 'App',
@@ -32,51 +24,20 @@ export default {
   },
   data() {
     return {
-      macroData: [],
       micro: []
     }
   },
   methods: {
-    // async fetchMacro() {
-    //   const res = await fetch('http://localhost:5000/api/functions');
-    //   const data = await res.json();
-    //   return data;
-    // },
-    // async fetchMicro(funcName) {
-    //   const res = await fetch(`http://localhost:5000/api/functions/${funcName}`);
-    //   const data = await res.json();
-    //   return data;
-    // },
-      async updateMacro() {
-        let order = macroJSON.order
-        delete macroJSON.order
-
-
-        console.log(order)
-
-        console.log(macroJSON)
-
-        // const renderer = new Renderer(container?: HTMLDivElement)
-        // const graph = parseJSON({
-          // nodes: [{id: 'kspacey'}, {id: 'swilliams'}, {id: 'kbacon'}, {id: 'bpitt'}, {id: 'hford'}, {id: 'lwilson'}],
-          // edges: [
-          //   {source: 'kspacey', target: 'swilliams'},
-          //   {source: 'swilliams', target: 'kbacon'},
-          //   {source: 'bpitt', target: 'kbacon'},
-          //   {source: 'lwilson', target: 'kbacon'},
-          // ],
-        // })
-        // renderer.setGraph(JSON.stringify(macroJSON))
-      }
+    
   },
-  async created() {
-    this.macroData = await this.updateMacro();
+  async mounted() {
+    this.micro = await fetch(fsdfs)
   }
 
 }
 </script>
 
-<style>
+<style scoped>
 * {
   box-sizing: border-box;
   margin: 0;
--- client/src/components/Macro.vue
+++ client/src/components/Macro.vue
@@ -1,15 +1,148 @@
 <template>
-  <div>
-    
+  <div class="macro-container">
+    <div id="cy"></div>
+    <div class="macro-player">
+      <button id="pause" class="playback-btn">Pause</button>
+      <button id="play" class="playback-btn">Play</button>
+    </div>
   </div>
+    
 </template>
 
 <script>
+import macroJSON from '../json/exampleData.json'
+import cytoscape from 'cytoscape';
+
 export default {
-  
+  name: 'Macro',
+  props: {
+  },
+  components: {
+    Function
+  },
+  data() {
+    return {
+      macroData: []
+    }
+  },
+  methods: {
+    async updateMacro() {
+      let cyto = []
+      Object.values(macroJSON.functions).forEach(val => {
+        const container = {};
+        container.data = {
+          id: val
+        };
+        cyto.push(container)
+      });
+      macroJSON.edges.forEach(edge => {
+        const container = {};
+        const edgeStart = macroJSON.functions[edge[0]];
+        const edgeEnd = macroJSON.functions[edge[1]];
+        container.data = {
+          id: edgeStart+ '-' + edgeEnd,
+          source: edgeStart,
+          target: edgeEnd
+        };
+        cyto.push(container)
+      });
+      return cyto
+    },
+    setUpGraph() {
+      let cy = cytoscape({
+      container: document.getElementById('cy'),
+      elements: this.macroData,
+      style: [ // the stylesheet for the graph
+          {
+            selector: 'node',
+            style: {
+              'background-color': '#666',
+              'label': 'data(id)'
+            },
+            css: {
+              events: 'no'
+            }
+          },
+          {
+            selector: 'edge',
+            style: {
+              'width': 3,
+              'line-color': '#ccc',
+              'target-arrow-color': '#ccc',
+              'target-arrow-shape': 'triangle',
+              'curve-style': 'bezier'
+            }
+          },
+          {
+            selector: '.foo',
+            style: {
+              'background-color': '#000'
+            }
+          }
+        ],
+      });
+      cy.userZoomingEnabled( false );
+
+      cy.on('tap', 'node', function (evt) {
+          // console.log(evt.target.id());
+          // console.log(evt.target);
+          // evt.target.toggleClass('foo')
+      });
+
+      let animationQueue= []
+      for(let i = 0; i < macroJSON.order.length; i++) {
+        if(i == macroJSON.order.length - 1) {
+          break
+        }
+        let sourceNode = cy.elements(`node#${macroJSON.order[i]}`);
+        let targetNode = cy.elements(`node#${macroJSON.order[i+1]}`);
+        let edge = sourceNode.edgesWith(`#${macroJSON.order[i+1]}`);
+        animationQueue.push(sourceNode.id());
+        animationQueue.push(edge.id());
+      }
+      console.log(animationQueue)
+
+      // this.animateNode(cy, 0);
+    },
+    animateNode(cy, source, edge, i) {
+      if (i >= macroJSON.order.length) {
+        return
+      }
+        const node = macroJSON.order[i];
+
+        let currentNode = cy.elements(`node#${node}`);
+        currentNode.animate({
+          style : {
+            'background-color' : 'yellow'
+          },
+          duration: 1000,
+          easing: 'linear'
+        }, 4000)
+
+        setTimeout(() => this.animateNode(cy, i + 1), 2500)
+      }
+  },
+  async mounted() {
+    this.macroData = await this.updateMacro();
+    await this.setUpGraph()
+  }
 }
 </script>
 
-<style>
-  
+<style scoped>
+#cy {
+  width: 50vw;
+  height: 50vh;
+  display: block;
+}
+.macro-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.playback-btn {
+  margin: 0 10px;
+}
 </style>
